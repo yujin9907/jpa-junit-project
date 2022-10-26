e3dc92969ae7748a4837108f405adcff1a809721
yujin9907 <heoyujin07@gmail.com>
2022-10-26 16:39:14 +0900

    커멘트 세이브 완료

--- src/main/java/site/metacoding/white/web/CommentAPIController.java
+++ src/main/java/site/metacoding/white/web/CommentAPIController.java
@@ -1,5 +1,29 @@
 package site.metacoding.white.web;
 
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import site.metacoding.white.dto.ResponseDto;
+import site.metacoding.white.dto.SessionUser;
+import site.metacoding.white.dto.CommentReqDto.CommentSaveReqDto;
+import site.metacoding.white.service.CommentService;
+
+@RequiredArgsConstructor
+@RestController
 public class CommentAPIController {
 
+    private final CommentService commentService;
+    private final HttpSession session;
+
+    @PostMapping("/comment")
+    public ResponseDto<?> saveComment(@RequestBody CommentSaveReqDto commentSaveReqDto) {
+        SessionUser user = (SessionUser) session.getAttribute("sessionUser");
+        commentSaveReqDto.setSessionUser(user);
+        return new ResponseDto<>(1, "성공", commentService.save(commentSaveReqDto));
+    }
 }
